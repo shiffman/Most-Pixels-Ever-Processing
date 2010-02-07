1014fcdccb163d8f3327e685f76886e9fd61d2df
Elie Zananiri <ez@silentlycrashing.net>
2010-02-07 14:41:02 +0000

    Completely re-wrote the OF client. It is now a true port of the P5 version and works exactly the same way.

--- mpeTCPClientOF/src/testApp.cpp
+++ mpeTCPClientOF/src/testApp.cpp
@@ -1,154 +1,85 @@
 #include "testApp.h"
 
 //--------------------------------------------------------------
-void testApp::setup(){
-
-	// initialize framerate
+void testApp::setup() {
+	// initialize app
 	ofSetFrameRate(30);
+    ofEnableSmoothing();
+    ofEnableAlphaBlending();
 	ofSetBackgroundAuto(false);
-
-	// set client's parent
-	client.setParent(*this);
+    
+	client.setup("settings.xml", this);
 	
-	// setup upd client
-	client.setup("settings.xml");
-	
+    // set the random seed
+	ofSeedRandom(1);
+    
+    // add a "randomly" placed ball
+    Ball* ball = new Ball(ofRandom(0, client.getMWidth()), ofRandom(0, client.getMHeight()), client.getMWidth(), client.getMHeight());
+    balls.push_back(ball);
+    
 	// start client
-	client.start();
-	
-	// set the random seed
-	ofSeedRandom(0);
-	
-	activeBallCount = 30;
-	maxBalls = 100;
-	bouncingBalls = new ball[maxBalls];
-	
-	for(int i=0; i<activeBallCount; i++){
-		
-		bouncingBalls[i].setParent(*this);
-		bouncingBalls[i].x = ofRandom(0, client.mWidth);
-		bouncingBalls[i].y = ofRandom(0, client.mHeight);
-		bouncingBalls[i].xdir = ofRandom(-5,5);
-		bouncingBalls[i].ydir = ofRandom(-5,5);
-	}
-
+    client.start();
 }
 
 //--------------------------------------------------------------
-void testApp::update(){	
-	client.update();
-	if(client.bDrawNewFrame) {
-		
-		if (client.messageAvailable()) {
-			
-			
-			string msg = client.getDataMessage();
-			printf("Received: %s",msg.c_str());
-			
-			
-			// if we haven't created the number of balls allocated
-			if(activeBallCount < maxBalls){
-				
-				
-				// find the location of the comma in the string
-				int commaPos = msg.find_first_of(",", 0);
-				
-				
-				// find the x and y position in the string
-				int ballXPos = atoi(msg.substr(0, commaPos).c_str());
-				int ballYPos = atoi(msg.substr(commaPos+1).c_str());
-				printf("creating new ball at %i %i\n", ballXPos, ballYPos);
-				
-				// start a new ball where the mouse was pressed, moving in a random direction
-				bouncingBalls[activeBallCount].setParent(*this);
-				bouncingBalls[activeBallCount].x = ballXPos;
-				bouncingBalls[activeBallCount].y = ballYPos;
-				bouncingBalls[activeBallCount].xdir = ofRandom(-5,5);
-				bouncingBalls[activeBallCount].ydir = ofRandom(-5,5);
-				
-				
-				// increment the ball count
-				activeBallCount++;
-			}
-		}
-		
-		// update ball positions
-		for(int i=0; i<activeBallCount; i++){
-			bouncingBalls[i].move();
-			bouncingBalls[i].bounce();
-		}
-	}
+void testApp::update() {	
 }
 
 //--------------------------------------------------------------
-void testApp::draw(){
-
-	if(client.bDrawNewFrame) {
-		// do all of your drawing here 
-		client.placeScreen();
-		// clear the screen
-		ofBackground(0, 0, 0);
-		for(int i=0; i<activeBallCount; i++){
-			//bouncingBalls[i].bounce();
-			bouncingBalls[i].draw();
-		}
-		client.bDrawNewFrame = false;
-		client.done();
-	}
-
-}
-//--------------------------------------------------------------
-
-void testApp::addBall(int x, int y){
-	
-	if(activeBallCount+1 < maxBalls){
-		
-		bouncingBalls[activeBallCount].setParent(*this);
-		bouncingBalls[activeBallCount].x = x;
-		bouncingBalls[activeBallCount].y = y;
-		bouncingBalls[activeBallCount].xdir = ofRandom(-5,5);
-		bouncingBalls[activeBallCount].ydir = ofRandom(-5,5);
-		
-		activeBallCount++;
-	}
-}
-
-
-//--------------------------------------------------------------
-void testApp::keyPressed(int key){
-
+void testApp::draw() {
 }
 
 //--------------------------------------------------------------
-void testApp::keyReleased(int key){
-
-
+void testApp::frameEvent() {
+    // clear the screen     
+    ofBackground(255, 255, 255);
+    
+    // move and draw all the balls
+    for (int i = 0; i < balls.size(); i++) {
+        balls[i]->calc();
+        balls[i]->draw();
+    }
+    
+    // read any incoming messages
+    if (client.messageAvailable()) {
+        vector<string> msg = client.getDataMessage();
+        vector<string> xy = ofSplitString(msg[0], ",");
+        float x = ofToInt(xy[0]);
+        float y = ofToInt(xy[1]);
+        Ball* ball = new Ball(x, y, client.getMWidth(), client.getMHeight());
+        balls.push_back(ball);
+    }
 }
 
 //--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
-
+void testApp::keyPressed(int key) {
 }
 
 //--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
-
+void testApp::keyReleased(int key) {
 }
 
 //--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
-	char msg[10];
-	sprintf(msg, "%d,%d", x+client.getXoffset(),y+client.getYoffset());
-	client.broadcast(msg);
+void testApp::mouseMoved(int x, int y) {
 }
 
 //--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
-
+void testApp::mouseDragged(int x, int y, int button) {
 }
 
 //--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
+void testApp::mousePressed(int x, int y, int button) {
+    // never include a ":" when broadcasting your message
+    x += client.getXoffset();
+    y += client.getYoffset();
+    client.broadcast(ofToString(x) + "," + ofToString(y));
+}
 
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button) {
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h) {
 }
 
--- mpeTCPClientOF/src/testApp.h
+++ mpeTCPClientOF/src/testApp.h
@@ -2,31 +2,30 @@
 #define _TEST_APP
 
 #include "ofMain.h"
-#include "tcpClient.h"
-#include "ball.h"
+#include "mpeClientTCP.h"
+#include "Ball.h"
 
-class testApp : public ofBaseApp{
+class testApp : public ofBaseApp, public mpeClientListener {
 
 	public:
-
-	void setup();
-	void update();
-	void draw();
-	
-	void keyPressed(int key);
-	void keyReleased(int key);
-	void mouseMoved(int x, int y );
-	void mouseDragged(int x, int y, int button);
-	void mousePressed(int x, int y, int button);
-	void mouseReleased(int x, int y, int button);
-	void windowResized(int w, int h);
-	
-	void addBall(int x, int y);
-	
-	tcpClient client;
-	ball * bouncingBalls;
-	int activeBallCount;
-	int maxBalls;
+        void setup();
+        void update();
+        void draw();
+        
+        void keyPressed(int key);
+        void keyReleased(int key);
+        void mouseMoved(int x, int y);
+        void mouseDragged(int x, int y, int button);
+        void mousePressed(int x, int y, int button);
+        void mouseReleased(int x, int y, int button);
+        void windowResized(int w, int h);
+        
+        void frameEvent();
+    
+    private:
+        mpeClientTCP  client;
+        vector<Ball*> balls;
+    
 };
 
 #endif
