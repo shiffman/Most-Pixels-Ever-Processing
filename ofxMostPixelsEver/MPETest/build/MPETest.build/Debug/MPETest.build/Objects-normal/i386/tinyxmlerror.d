321d7e5fc50af694149add34cce59f00f54766a9
Adria Navarro <araidz@gmail.com>
2012-12-05 23:46:54 -0500

    cleaned build directory

--- ofxMostPixelsEver/MPETest/build/MPETest.build/Debug/MPETest.build/Objects-normal/i386/tinyxmlerror.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/ad/OpenFrameworks/of_v0072_osx_release/Most-Pixels-Ever/ofxMostPixelsEver/MPETest/../../../addons/ofxXmlSettings/libs/tinyxmlerror.cpp \
-  /Users/ad/OpenFrameworks/of_v0072_osx_release/Most-Pixels-Ever/ofxMostPixelsEver/MPETest/../../../addons/ofxXmlSettings/libs/tinyxml.h
--- ofxMostPixelsEver/MPETest/build/MPETest.build/Debug/client2.build/Objects-normal/i386/tinyxml.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/ad/OpenFrameworks/of_v0072_osx_release/Most-Pixels-Ever/ofxMostPixelsEver/MPETest/../../../addons/ofxXmlSettings/libs/tinyxml.cpp \
-  /Users/ad/OpenFrameworks/of_v0072_osx_release/Most-Pixels-Ever/ofxMostPixelsEver/MPETest/../../../addons/ofxXmlSettings/libs/tinyxml.h
