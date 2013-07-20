76ba3af1e25d0e57a9b023d897bef7bada5e54fd
Daniel Shiffman <daniel@shiffman.net>
2013-07-20 12:36:43 -0400

    refactoring to use new Processing library template

---
+++ Most-Pixels-Ever-Processing/readme.txt
@@ -0,0 +1,1 @@
+Up-to-date instructions can be found at https://github.com/processing/processing-templates/wiki/Eclipse-Library-Template
---
+++ Most-Pixels-Ever-Processing/src/template/library/HelloLibrary.java
@@ -0,0 +1,104 @@
+/**
+ * ##library.name##
+ * ##library.sentence##
+ * ##library.url##
+ *
+ * Copyright ##copyright## ##author##
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA  02111-1307  USA
+ * 
+ * @author      ##author##
+ * @modified    ##date##
+ * @version     ##library.prettyVersion## (##library.version##)
+ */
+
+package template.library;
+
+
+import processing.core.*;
+
+/**
+ * This is a template class and can be used to start a new processing library or tool.
+ * Make sure you rename this class as well as the name of the example package 'template' 
+ * to your own library or tool naming convention.
+ * 
+ * @example Hello 
+ * 
+ * (the tag @example followed by the name of an example included in folder 'examples' will
+ * automatically include the example in the javadoc.)
+ *
+ */
+
+public class HelloLibrary {
+	
+	// myParent is a reference to the parent sketch
+	PApplet myParent;
+
+	int myVariable = 0;
+	
+	public final static String VERSION = "##library.prettyVersion##";
+	
+
+	/**
+	 * a Constructor, usually called in the setup() method in your sketch to
+	 * initialize and start the library.
+	 * 
+	 * @example Hello
+	 * @param theParent
+	 */
+	public HelloLibrary(PApplet theParent) {
+		myParent = theParent;
+		welcome();
+	}
+	
+	
+	private void welcome() {
+		System.out.println("##library.name## ##library.prettyVersion## by ##author##");
+	}
+	
+	
+	public String sayHello() {
+		return "hello library.";
+	}
+	/**
+	 * return the version of the library.
+	 * 
+	 * @return String
+	 */
+	public static String version() {
+		return VERSION;
+	}
+
+	/**
+	 * 
+	 * @param theA
+	 *          the width of test
+	 * @param theB
+	 *          the height of test
+	 */
+	public void setVariable(int theA, int theB) {
+		myVariable = theA + theB;
+	}
+
+	/**
+	 * 
+	 * @return int
+	 */
+	public int getVariable() {
+		return myVariable;
+	}
+}
+
